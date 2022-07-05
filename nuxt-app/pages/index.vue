799964e40915be6d7610f6595d667218c4f280b3
Nicola Solazzo <solazzo@logixcorp.it>
2022-07-05 19:24:51 +0200

    add test

--- nuxt-app/pages/index.vue
+++ nuxt-app/pages/index.vue
@@ -2,10 +2,46 @@
   const info = useInfo()
   const a = useA()
   const md = mergeData(a, info)
+
+  const countRef = ref(0)
+  const incrementRef = () => {
+    if(countRef.value === 10) return
+    countRef.value++
+  }
+  const decrementRef = () => {
+    if(countRef.value === 0) return
+    countRef.value--
+  }
+
+  const countReactive = reactive({
+    value: 0
+  })
+  const incrementReactive = () => {
+    if(countReactive.value === 10) return
+    countReactive.value++
+  }
+  const decrementReactive = () => {
+    if(countReactive.value === 0) return
+    countReactive.value--
+  }
 </script>
 
 <template>
   <div>
+    <div>
+      REF:
+      <h1>{{countRef}}</h1>
+      <button @click="incrementRef"><h3>+</h3></button>
+      <button @click="decrementRef"><h3>-</h3></button>
+    </div>
+    <hr>
+    <div>
+      REACTIVE
+      <h1>{{countReactive.value}}</h1>
+      <button @click="incrementReactive"><h3>+</h3></button>
+      <button @click="decrementReactive"><h3>-</h3></button>
+    </div>
+    <hr>
     <NuxtLink to="/about">
       <button>About</button>
     </NuxtLink>
